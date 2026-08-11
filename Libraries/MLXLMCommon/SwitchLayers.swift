5512f3e0dfb69820d94f6872ee878947331ebcd4
figyaro <mssupermind@gmail.com>
2026-08-11 23:21:12 +0700

    fix: guard MXFP8 decode dimensions

--- Libraries/MLXLMCommon/SwitchLayers.swift
+++ Libraries/MLXLMCommon/SwitchLayers.swift
@@ -225,6 +225,8 @@ public class FusedGateUpSwitchGLU: Module {
         if let decodeQuantization,
             !(gateUpProj is QuantizedSwitchLinear),
             !(downProj is QuantizedSwitchLinear),
+            inputDims % decodeQuantization.groupSize == 0,
+            hiddenDims % decodeQuantization.groupSize == 0,
             !doSort, gateUpProj.bias == nil, downProj.bias == nil,
             indices.dim(0) == 1, indices.dim(1) == 1 {
             decodeQuantizedWeightsLock.lock()
